97366ee575434938c26f73075b3a4e82d655d08e
LiuZiQiao <1844861420@qq.com>
2019-03-04 21:34:56 +0800

    update

---
+++ ChatSystem2/ChatClient.hpp
@@ -0,0 +1,178 @@
+#pragma ocne 
+#include"Socket.hpp"
+#include"json/json.h"
+#include"Util.hpp"
+#include"Message.hpp"
+
+#define TCP_PORT 8080
+#define UDP_PORT 8888
+
+class ChatClient
+{
+private:
+    std::string ip;
+    int tcp_sock;
+    int udp_sock;
+
+    unsigned int id;
+    std::string nick_name;
+    std::string school;
+    std::string passwd;
+    struct sockaddr_in server;
+public:
+    ChatClient(std::string ip_):ip(ip_)
+    {
+        id=0;
+        tcp_sock = -1;
+        udp_sock = -1;
+        server.sin_family=AF_INET;
+        server.sin_addr.s_addr=inet_addr(ip.c_str());
+        server.sin_port=htons(UDP_PORT);
+    }
+
+    void InitClient()
+    {
+        udp_sock = SocketApi::Socket(SOCK_DGRAM);
+    }
+
+    bool ConnectServer()
+    {
+        tcp_sock = SocketApi::Socket(SOCK_STREAM);
+        return SocketApi::Connect(tcp_sock,ip,TCP_PORT);
+    }
+
+	bool RegisterEnter(std::string &nick_name,std::string &school,std::string &password)
+	{
+		std::cout<<"Please Input Nick Name:";
+		std::cin>> nick_name;
+		std::cout<<"Please Input School:";
+		std::cin>>school;
+		std::cout<<"Please Input password:";
+		std::cin>>password;
+		std::string again;
+		std::cout<<"Please Input password again:";
+		std::cin>>again;
+		if(again == password)
+		{
+			return true;
+		}
+		return false;
+	}
+
+    bool Register(){
+        if(RegisterEnter(nick_name,school,passwd) && ConnectServer()){
+            
+            Request rq;
+            rq.method ="REGISTER\n";
+            
+            Json::Value root;
+            root["name"] = nick_name;
+            root["school"] = school;
+            root["passwd"] = passwd;
+
+            Util::Serializ(root,rq.text);
+
+		    std::cout<<"RegisterEnter rq.text:"<<rq.text<<std::endl;
+
+            rq.content_length = "Content_length:";
+            rq.content_length +=  Util::IntToString((rq.text).size());
+            rq.content_length += "\n";
+            SocketApi::SendRequest(tcp_sock,rq);    //send request
+            recv(tcp_sock,&id,sizeof(id),0);   // response; return  a register id;
+
+            bool res =false;
+            if(id >= 10000){
+                std::cout<<"Register Success! Your Login ID is:"<<id<<std::endl;
+                res = true;
+            }else{
+                std::cout<<"Register Faild!"<<std::endl; 
+            }
+            close(tcp_sock);
+            return res;
+        }
+    }
+
+    bool LoginEnter(unsigned int &id,std::string &password)
+    {
+        std::cout<<"Login ID:";
+		std::cin>>id;
+		std::cout<<"Password:";
+        std::cin>>passwd;
+        return true;
+    }
+
+    bool Login()
+    {
+        if(LoginEnter(id,passwd) && ConnectServer()){
+            
+            Request rq;
+            rq.method ="LOGIN\n";
+            
+            Json::Value root;
+            root["id"] = id;
+            root["passwd"] = passwd;
+
+            Util::Serializ(root,rq.text);
+
+            rq.content_length = "Content_length:"+Util::IntToString((rq.text).size());
+            rq.content_length += "\n";
+            SocketApi::SendRequest(tcp_sock,rq);
+           
+            // std::string resutl;
+            unsigned int result;
+            recv(tcp_sock,&result,sizeof(result),0);      //recv the login result,and result include the user info
+
+            // Json::Value rs;
+            // Util::UnSerializ(resutl,rs);
+            // nick_name = rs["name"].asString();
+            // school = rs["school"].asString();
+
+            bool res = false;
+            if(result >= 10000){
+                std::cout<<"Login Success!"<<id<<std::endl;
+                res = true;
+            }else{
+                std::cout<<"Login Faild! Code is"<<result<<std::endl; 
+            }
+            close(tcp_sock);
+            return res;
+        }
+    }
+
+    void Chat()
+    {
+
+            /*以下注释代码 用以上代码 替换*/
+            // std::cout<<"Input you name:";
+            // std::cin>>name;
+            // std::cout<<"Input shool :";
+            // std::cin>>school;
+
+            Message msg;
+            while(1){
+                std::string text;
+                std::cout<<"Enter>>";
+                std::cin>>text;
+                Message msg(nick_name,school,text,id);
+                std::string sendString;
+                msg.ToSendString(sendString);       //将发送的消息转化成要发送的字符串
+                
+                // std::cout<<"debug ToSendString:"<<sendString<<std::endl;
+
+                Util::SendMessage(udp_sock,sendString,server);  //send to server
+
+                std::string recvString;
+                struct sockaddr_in peer;
+
+                Util::RecvMessage(udp_sock,recvString,peer);      // recv message from server
+                std::cout<<"clien RecvMsg Debug:"<<recvString<<std::endl;
+                msg.ToRecvValue(recvString);    //将消息序列化到message对象中
+                // std::cout<<"name:"<<msg.Nick_Name()<<std::endl;
+                // std::cout<<"school:"<<msg.School()<<std::endl;
+                // std::cout<<"Text:"<<msg.Text()<<std::endl;
+            }
+   
+    }
+
+    ~ChatClient(){}
+};
---
+++ ChatSystem2/Log.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+#define LOG(msg,level) Log(msg,level,__FILE__,__LINE__)
+
+#define NORMAL 1
+#define WARNING 2
+#define ERROR 3
+
+const char *log_level[]={
+    NULL,
+    "Normal",
+    "Woring"
+    "Error"
+};
+
+void Log(std::string msg,int level,char* file,int line)
+{
+    std::cout<<'['<<file<<']'<<":"<<line<<":"<<msg<<"-"<<log_level[level]<<std::endl;
+}
